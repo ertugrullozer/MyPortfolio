d0fd0210b1992a59042587744ec74ecd5964e77d
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-12 22:20:54 +0300

    View Componentlerin oluşturulması

---
+++ MyPortfolio/ViewComponents/_ExperianceComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _ExperianceComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { 
+            return View();
+                }
+    }
+}
---
+++ MyPortfolio/ViewComponents/_PortfolioComponenetPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _PortfolioComponenetPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
