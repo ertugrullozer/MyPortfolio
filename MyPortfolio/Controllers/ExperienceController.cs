f32e0688cd69327cb1e783bdf162ce3ef86d2327
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-13 10:20:30 +0300

    Deneyimler Bölümünde CRUD İşlemleri Uygulandı

---
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values= context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperiance()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperiance(Experience experience) 
+        { 
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+     
+        public IActionResult DeleteExperience(int id) 
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperiance(int id)
+        {
+            var value = context.Experiences.Find(id);
+          return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperiance(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+
+
+
+	}
+}
