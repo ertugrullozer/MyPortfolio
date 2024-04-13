4caae121991ac4aeb73e2aa62cc559c3a6e0d38d
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-13 22:43:59 +0300

    About ve Feautre Tamamlandı

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class AboutController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult AboutList()
+		{
+			var values= context.Abouts.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult CreateAbout() 
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CreateAbout(About about) 
+		{
+			context.Abouts.Add(about);
+			context.SaveChanges();
+			return RedirectToAction("AboutList");
+
+		}
+
+		public IActionResult DeleteAbout(int id) 
+		{
+
+			var value = context.Abouts.Find(id);
+			context.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("AboutList");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateAbout(int id) 
+		{
+			var value = context.Abouts.Find(id);
+			return View(value);
+		}
+		[HttpPost]
+		public IActionResult UpdateAbout(About about)
+		{
+			context.Update(about);
+			context.SaveChanges();
+			return RedirectToAction("AboutList");
+		}
+	}
+}
