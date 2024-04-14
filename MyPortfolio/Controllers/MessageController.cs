39c6501af2d7c8286f6e93171e1e070a6b67ea44
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-14 13:49:09 +0300

    Message controllerin yazılması

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class MessageController : Controller
+	{
+		MyPortfolioContext context= new MyPortfolioContext();
+		public IActionResult Inbox()
+		{
+			var value=context.Messages.ToList();
+			return View(value);
+		}
+		public IActionResult ChangeIsReadToTrue(int id) 
+		{
+			var value = context.Messages.Find(id);
+			value.IsRead=true;
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+		public IActionResult ChangeIsReadToFalse(int id) 
+		{
+			var value= context.Messages.Find(id);
+			value.IsRead= false;
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult DeleteMessage(int id)
+		{
+			var value = context.Messages.Find(id);
+			context.Messages.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+		public IActionResult GetMessage(int id) 
+		{
+			var value = context.Messages.Find(id);
+			return View(value);
+		}
+
+
+
+	}
+}
